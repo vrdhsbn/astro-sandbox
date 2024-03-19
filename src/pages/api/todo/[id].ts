ccec853d9f0aad95f156fbb8614d868184f32fee
Naoki Komazawa <komazawa@ekzm.co.jp>
2024-03-19 17:28:11 +0900

    commit

--- src/pages/api/todo/[id].ts
+++ src/pages/api/todo/[id].ts
@@ -1,17 +1,22 @@
 import type { APIRoute } from 'astro'
 import { db, eq, Todo } from 'astro:db'
 
+// チェックボックスのON/OFFを切り替える処理
 export const POST: APIRoute = async ({ params, request, redirect }) => {
   const formData = await request.formData()
   const id = params.id as unknown as number
   const _finish = formData.get('finish')
   const finish = _finish === 'on' ? true : false
 
-
-  console.log(finish)
-  console.log(params.id)
-
   await db.update(Todo).set({ finish }).where(eq(Todo.id, id))
 
   return new Response(null, {status: 204})
+}
+
+// Todoを削除する処理
+export const GET: APIRoute = async ({ params, redirect }) => {
+  // 型キャストはこういう書き方もできるのか。
+  await db.delete(Todo).where(eq(Todo.id, Number(params.id)))
+  
+  return redirect('/')
 }
