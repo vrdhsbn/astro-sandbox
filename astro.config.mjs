ddf610d4e5256ea1f16bcf705ec5c0d49b4a9fc2
Naoki Komazawa <komazawa@ekzm.co.jp>
2024-09-27 11:48:53 +0900

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,19 +5,26 @@ import { defineConfig } from 'astro/config'
 
 import db from '@astrojs/db'
 
+import vercel from '@astrojs/vercel/serverless'
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   server: {
     open: true,
   },
+
   devToolbar: {
     enabled: false,
   },
+
   build: {
     // format: 'file',
     inlineStylesheets: 'never',
   },
+
   compressHTML: false,
   integrations: [react(), compress({ CSS: false, Image: false, SVG: false }), db()],
+  adapter: vercel(),
 })
